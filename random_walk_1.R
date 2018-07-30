4963e8e91345fb2eaee64fc367210f3240d7b519
vedantja <vedjain85@gmail.com>
2018-07-30 01:50:53 -0500

    acf of random walk

--- random_walk_1.R
+++ random_walk_1.R
@@ -3,4 +3,11 @@ x[1] = 0
 for(i in 2:1000){
   x[i]=x[i-1]+rnorm(1)
 }
-print(x)
+
+random_walk = ts(x)
+# plot(random_walk, main='A random walk', xlab = 'Days', col='blue', lwd=2)
+
+# Let's find the acf of random walk dataset
+
+acf(random_walk)
+acf(diff(random_walk))
